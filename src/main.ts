46534e7149e66fed570993c74201c4e619bb6b9a
TSepron <proffizitdollara@gmail.com>
2022-07-24 19:59:44 +0300

    Set up db in docker for our app

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as YAML from 'yamljs';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 dotenv.config();
+import { Client } from 'pg';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,21 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT || 4000);
+
+  console.log(process.env.DATABASE_URL);
+
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_DB,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: Number(process.env.PGPORT),
+  });
+  client.connect((err) => console.log(err));
+  client.query('SELECT NOW()', (err, res) => {
+    console.log(err, res);
+    client.end();
+  });
 }
 
 bootstrap();
