dcb28ca79a559128d26f97593f3a9b77b725fab5
TSepron <proffizitdollara@gmail.com>
2022-08-07 18:29:49 +0300

    Implement Exception Filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import * as YAML from 'yamljs';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggerService } from './logger/logger.service';
+import { ExceptionFilter } from './custom-exception.filter';
 dotenv.config();
 
 async function bootstrap() {
@@ -15,6 +16,8 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(LoggerService));
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new ExceptionFilter(httpAdapter));
 
   await app.listen(process.env.PORT || 4000);
 }
